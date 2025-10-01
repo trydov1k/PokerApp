7e51b7f2ba0593828dbe803195e59c5d92cb73ca
trydov1k <mdimon00@mail.ru>
2025-10-01 19:56:36 +0500

    Дополнил окно с меню выборов ржимов

--- MenuForm.Designer.cs
+++ MenuForm.Designer.cs
@@ -31,11 +31,16 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.closeLabel = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.closeLabel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -59,10 +64,44 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(181, 248);
+            this.comboBox1.Items.AddRange(new object[] {
+            "100 200 300 400 500",
+            "100 200 400 500 800 ",
+            "100 200 400 800 1600",
+            "200 400 800 1600"});
+            this.comboBox1.Location = new System.Drawing.Point(146, 126);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(149, 21);
             this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(35, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(430, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Меню режимов изменения блайндов";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(35, 40);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 506);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
@@ -70,6 +109,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGreen;
             this.ClientSize = new System.Drawing.Size(500, 600);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -85,5 +125,8 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label closeLabel;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
     }
 }
